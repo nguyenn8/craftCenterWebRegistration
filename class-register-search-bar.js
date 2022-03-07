79514f939d3b26ee14d8f93742be1f3649ed303f
nguyenn8 <nguyenn8@oregonstate.edu>
2022-03-07 15:24:40 -0800

    created js for search bar

--- class-register-search-bar.js
+++ class-register-search-bar.js
@@ -1,3 +1,5 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+
 const classes = []
 classes.push({
     "className": "Beginning Wheel Throwing Mondays Session A",
@@ -11,32 +13,45 @@ classes.push({
 })
 
 classes.push({
-    "className": "d",
-    "category": "e",
-    "instructor": "f",
+    "className": "Make a Mug Workshop!",
+    "category": "Ceramics",
+    "instructor": "Rachel Kirby (she/her)",
+    "term": "Winter 2022",
+    "fee": "$20",
+    "max enrl": 2,
+    "actual enrl": 1,
+    "description": "Make your own hand built mug while learning handbuilding techniques!"
 })
 
 const myJSON = JSON.stringify(classes);
 
 let data = JSON.parse(myJSON)
-console.log(data)
+// console.log(data)
 
-function search_animal() {
-  let input = document.getElementById('searchbar').value
-  input = input.toLowerCase();
-  let x = document.querySelector('#classesList');
-  x.innerHTML = ""
+data.forEach(classes => {
+  const card = userCardTemplate.content.cloneNode(true)
+  const collapsible = card.querySelector("[collapsible-header]")
+  console.log(classes)
 
-  for (i = 0; i < data.length; i++) {
-    let obj = data[i];
+});
 
-    if (obj.className.toLowerCase().includes(input)) {
-      const elem = document.createElement("li")
-      elem.innerHTML = `${obj.className} - ${obj.category} - ${obj.instructor}`
-      x.appendChild(elem)
-    }
-  }
-}
+
+// function search_animal() {
+//   let input = document.getElementById('searchbar').value
+//   input = input.toLowerCase();
+//   let x = document.querySelector('#list-holder');
+//   x.innerHTML = "";
+//
+//   for (i = 0; i < data.length; i++) {
+//     let obj = data[i];
+//
+//     if (obj.className.toLowerCase().includes(input)) {
+//       const elem = document.createElement("li")
+//       elem.innerHTML = `${obj.className} - ${obj.category} - ${obj.instructor}`
+//       x.appendChild(elem)
+//     }
+//   }
+// }
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
